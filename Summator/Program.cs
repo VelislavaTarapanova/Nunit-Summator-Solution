4bad6a23ec069dd3d764dadb4f47d9e7e5b21993
VelislavaTarapanova <105131409+VelislavaTarapanova@users.noreply.github.com>
2022-05-19 23:01:55 +0300

    Add files

---
+++ Summator/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 10, 20, 30 });
+
+            if (result == 60)
+            {
+                Console.WriteLine("THE TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("THE TEST FAIL");
+            }
+
+            int averageResult = Summator.Average(new int[] { 10, 20, 30 });
+
+            if (averageResult == 20)
+            {
+                Console.WriteLine("THE TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("THE TEST FAIL");
+            }
+
+            int evenSum = Summator.EvenNumbers(new int[] { 2, 7, 10, 17, 28, 33, 46, 148, 232 });
+
+            if (evenSum == 466)
+            {
+                Console.WriteLine("THE TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("THE TEST FAIL");
+            }
+
+        }
+    }
+}
+
+
+
